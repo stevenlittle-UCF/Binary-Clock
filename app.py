1654c52a7001f299ec1a502e3a1e3816f1f8b791
Steven <stevenmlittle@knights.ucf.edu>
2020-04-05 14:36:42 +0000

    Move servers

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 
 tz = pytz.timezone('US/Eastern')
 
+ClockIP = "http://192.168.2.252"
+PlexIP = "http://192.168.2.10:32400/web"
+
 @app.route('/time', methods=['GET'])
 def time():
     time=datetime.now(tz)
@@ -13,5 +16,5 @@ def time():
 
 @app.route('/')
 def home():
-    html = "<h1>Please go to clock IP to manipulate the clock!"
+    html = "<h1>Please go to clock IP to manipulate the clock!</h1><form action = " + ClockIP + "><button type = \"submit\">Clock Server</button></form><h2>For Plex server click below</h2><form action = " + PlexIP + "><button type = \"submit\">Plex Server</button></form>"
     return html
---
+++ readme.txt
@@ -0,0 +1,5 @@
+to go into venv:
+source venv/bin/activate
+
+run server cmd:
+guincorn -w 3 app:app
